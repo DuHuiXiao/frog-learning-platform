88859c2382d01aaccf14fe57a1019908ed5f9e44
ranlongjun <ranlongjun@bytedance.com>
2025-05-17 16:19:46 +0800

    fix: 正常
    Change-Id: Ie17209893be1f2fe72bd76030856ed46ebe6cee0

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import MainHeader from '@/components/MainHeader';
 import Config from '@/components/Config';
 
 export default function RootLayout({
-  children,sssaaa
+  children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {

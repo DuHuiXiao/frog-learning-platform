0d19fafb018347438e7a9050a37f854695f809ac
ranlongjun <ranlongjun@bytedance.com>
2025-05-17 15:00:28 +0800

    fix: 首页闪屏问题修复
    Change-Id: Ibba11f01c4926ea81ab12fec0856136ad8cd3dcf

--- src/components/Config/index.tsx
+++ src/components/Config/index.tsx
@@ -1,11 +1,8 @@
 'use client'
-
-import { initRemUnit } from "@/util/rem";
 import { useEffect } from "react";
 
 export default function Config() {
   useEffect(()=>{
-    initRemUnit();
   })
   return (null);
 }

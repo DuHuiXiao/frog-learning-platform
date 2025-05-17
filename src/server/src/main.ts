7935706c116d827bd53d1ff82e3fcb89ae3e2be3
ranlongjun <ranlongjun@bytedance.com>
2025-05-18 00:57:47 +0800

    chore: 改端口
    Change-Id: I6a4830975260b2a9108711da3dd319b99c7304b8

--- src/server/src/main.ts
+++ src/server/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const port = 3000;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(port);
+  console.log(`nest 服务器已启动，监听端口： ${port}`);
 }
-bootstrap();
+
+void bootstrap();
